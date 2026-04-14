7cd2c02f4ba3e3a58b0b7ab38f61e064f10e54ed
Aparna Mounika Anupoju <manupoju@msa.miraclesoft.com>
2026-04-14 15:46:51 +0530

    Updated Code

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 USERS = [
     {"id": 1, "name": "Aparna", "role": "DevOps Engineer"},
-    {"id": 2, "name": "John", "role": "Backend Developer"},
-    {"id": 3, "name": "Sara", "role": "QA Engineer"}
+    {"id": 2, "name": "Rohit", "role": "Cloud and DevOps Engineer"},
+    {"id": 3, "name": "Akhila", "role": "Jr Technical Associate"},
+    {"id": 4, "name": "Manu", "role": "Jr Technical Architect"},
 ]
