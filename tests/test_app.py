efec764235b3276a8c77da9e8a0d795fa51e352c
Aparna Mounika Anupoju <manupoju@msa.miraclesoft.com>
2026-04-14 17:10:55 +0530

    Updated Code

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,8 +4,23 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+    assert response.json["message"] == "CI/CD Demo App is running"
 
 def test_users():
     client = app.test_client()
     response = client.get("/users")
     assert response.status_code == 200
+    assert isinstance(response.json, list)
+    assert len(response.json) > 0
+
+def test_single_user():
+    client = app.test_client()
+    response = client.get("/users/1")
+    assert response.status_code == 200
+    assert response.json["name"] == "Aparna"
+
+def test_user_not_found():
+    client = app.test_client()
+    response = client.get("/users/999")
+    assert response.status_code == 404
+    assert b"User not found" in response.data
